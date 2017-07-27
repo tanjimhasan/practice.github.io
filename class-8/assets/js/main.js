b319ed8040ab1bc9a5054d2d32d5d5fd5ef2d77f
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-07-28 01:54:45 +0600

    work for class 8

---
+++ class-8/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".product-carousel").owlCarousel({
+    		item: 4,
+    		margin: 30,
+    		autoplay: true,
+    		loop: true,
+    		nav: true,
+    		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+    	})
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
