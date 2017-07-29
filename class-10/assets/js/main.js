ec4de122d22218cb9398d6869e7ce0236181c110
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-07-30 00:55:29 +0600

    work for class 10

---
+++ class-10/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".homepage-slider").owlCarousel({
+    		items: 1,
+    		loop: true,
+    		nav: true,
+            autoplay: true,
+    		navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"]
+    	})
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
