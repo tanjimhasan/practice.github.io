6edd20f9f1d0850760deeea0384ca728b14f49c4
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-08-04 17:56:19 +0600

    class-13

---
+++ class-13/assets/js/main.js
@@ -0,0 +1,47 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+ 
+    	var hopageSlides= $(".homepage-slides");
+
+    	hopageSlides.owlCarousel({
+    		items: 1,
+    		loop: true,
+    		autoplay: true,
+    		nav: true,
+    		navText: ["<i class='fa fa-arrow-left'></i>","<i class='fa fa-arrow-right'></i>"]
+    	});
+
+    	hopageSlides.on('translated.owl.carousel', function(event) {
+    	    $(".slider-text h2").css('color','#01C0DA');
+    	});
+
+    	hopageSlides.on('translate.owl.carousel', function(event) {
+    		$(".slider-text p").removeClass("animated slideInRight");
+    	    $(".slider-text h2").removeClass("animated jackInTheBox");
+    	});
+
+    	hopageSlides.on('translated.owl.carousel', function(event) {
+    		$(".slider-text p").addClass("animated slideInRight");
+    	    $(".slider-text h2").addClass("animated jackInTheBox");
+    	   
+    	});
+
+       	$(".video-bg-area").YTPlayer({
+       	    fitToBackground: true,
+       	    videoId: 'LSmgKRx5pBo'
+       	});
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
