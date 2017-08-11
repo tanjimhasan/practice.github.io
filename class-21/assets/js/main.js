797bb73adbd50c8bd09827a157b316fe313fadcf
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-08-11 22:25:14 +0600

    class-21 done

---
+++ class-21/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(document).ready(function(){
+    	    $('.schedule').DataTable();
+    	});
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
