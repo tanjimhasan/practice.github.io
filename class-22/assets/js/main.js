3b18001eec5503d4fd50b41d8a37c27cb59e9621
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-08-13 16:35:21 +0600

    class 22 done!

---
+++ class-22/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".project-items").owlCarousel({
+    		items: 5,
+    		loop: true,
+    		dots: true,
+    		nav: true,
+    		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+    	});
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
