b057151c76033975ff5c71b7e8b68bedc8287aa9
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-08-26 18:29:31 +0600

    class 28 done!

---
+++ class-28/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".portfolio-list").masonry({
+    		percentPosition: true,
+    		fitWidth: true,
+    	});
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+        
+    });
+
+
+}(jQuery));
