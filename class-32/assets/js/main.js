81946630b637d7baf54c3963fe89099ee15e3bea
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-09-25 15:10:00 +0600

    class 32 done!

---
+++ class-32/assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".vdo-play-btn").modalVideo();
+
+    	$(".vdo-play-btn").on("click",(function(event) {
+    		return false;
+    	});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
