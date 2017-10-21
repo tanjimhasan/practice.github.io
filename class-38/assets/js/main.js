4ab19091521266b88123e0304e2e94c6bbc5248c
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-10-21 15:08:13 +0600

    class-38 done!

---
+++ class-38/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$('.count').counterUp({
+            delay: 10,
+            time: 1000
+       	});
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
