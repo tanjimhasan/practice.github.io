9e7a7f303c970472299a6d44f6348a110b3e6fb6
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2017-10-29 15:16:59 +0600

    class- 48 done!

---
+++ class-48/assets/js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+    	$(".slider-area").owlCarousel({
+    		items: 1,
+    		loop: true,
+    		autoplay: true,
+    		nav: false,
+    		dots: true,
+    	});
+
+    });
+
+
+}(jQuery));
